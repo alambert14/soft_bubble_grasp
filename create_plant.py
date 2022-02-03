a08fef19a605811b4e8dc4125c043937e9988f1e
Andy Lambert <andyalambert@gmail.com>
2022-02-03 11:51:15 -0500

    It compiles :')

---
+++ create_plant.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+
+import pydrake.common
+from pydrake.all import (
+    MultibodyPlant, Parser, ProcessModelDirectives, LoadModelDirectives,
+    SpatialInertia, FindResourceOrThrow, RigidTransform,
+)
+from pydrake.math import RollPitchYaw
+
+models_dir = os.path.join(
+    os.path.dirname(os.curdir), 'models')
+
+bubble_sdf_file = 'models/bubble_gripper/schunk_wsg_50_bubble_collision.sdf'
+
+
+# L7 is link 7, E is end-effector
+X_L7E = RigidTransform(
+    RollPitchYaw(np.pi / 2, 0, np.pi / 2), np.array([0, 0, 0.114]))
+
+def add_package_paths(parser: Parser):
+    parser.package_map().Add(
+        "drake_manipulation_models",
+        os.path.join(pydrake.common.GetDrakePath(),
+                     "manipulation/models"))
+
+    parser.package_map().Add("iiwa_controller", models_dir)
+
+def create_iiwa_soft_bubble_plant(gravity):
+    plant = MultibodyPlant(time_step=1e-3)
+    parser = Parser(plant=plant)
+    add_package_paths(parser)
+    ProcessModelDirectives(
+        LoadModelDirectives(os.path.join(models_dir, 'iiwa.yml')),
+        plant, parser)
+    plant.mutable_gravity_field().set_gravity_vector(gravity)
+
+    gripper_plant = MultibodyPlant(1e-3)
+    gripper_parser = Parser(gripper_plant)
+    gripper_parser.AddModelFromFile(bubble_sdf_file)
+    gripper_plant.Finalize()
+
+    iiwa_model = plant.GetModelInstanceByName('iiwa')
+    bubble_gripper = plant.AddRigidBody(
+        'soft_bubble', iiwa_model, SpatialInertia())
+    plant.WeldFrames(
+        frame_on_parent_P=plant.GetFrameByName('iiwa_link_7', iiwa_model),
+        frame_on_child_C=bubble_gripper.body_frame(),
+        X_PC=X_L7E)
+
+    plant.Finalize()
+    link_frame_indices = []
+    for i in range(8):
+        link_frame_indices.append(
+            plant.GetFrameByName("iiwa_link_" + str(i)).index())
+
+    return plant, link_frame_indices
--- main.py
+++ main.py
@@ -1,60 +1,117 @@
+import os
 import numpy as np
+
+import graphviz
+import pydrake.common
 from pydrake.all import (
     AddMultibodyPlantSceneGraph, MeshcatVisualizerCpp, DiagramBuilder, Integrator,
-    LeafSystem, MultibodyPlant, PiecewisePose, Quaternion, RigidTransform, RollPitchYaw,
+    LeafSystem, MultibodyPlant, PiecewisePose, Quaternion, RigidTransform,
     RotationMatrix, SceneGraph, Simulator, TrajectorySource, MeshcatVisualizerParams, ConstantVectorSource,
-    ConnectMeshcatVisualizer,
+    ConnectMeshcatVisualizer, InverseDynamicsController, Parser, ProcessModelDirectives, LoadModelDirectives,
+    ZeroOrderHold,
 )
+from pydrake.math import RollPitchYaw
 from pydrake.examples.manipulation_station import ManipulationStation
 # from manipulation.meshcat_cpp_utils import StartMeshcat, AddMeshcatTriad
-
 import meshcat
 
-class BubbleGripperManipulator():
+from create_plant import create_iiwa_soft_bubble_plant
+from manipulation.utils import AddPackagePaths
+
+
+def add_package_paths_local(parser: Parser):
+    models_dir = os.path.join(os.curdir, 'models')
+    iiwa_controller_models_dir = os.path.join(
+        os.path.dirname(os.curdir), 'models')
+
+    parser.package_map().Add(
+        "drake_manipulation_models",
+        os.path.join(pydrake.common.GetDrakePath(),
+                     "manipulation/models"))
+    parser.package_map().Add("local", models_dir)
+    parser.package_map().Add('iiwa_controller',
+                             iiwa_controller_models_dir)
+    parser.package_map().PopulateFromFolder(models_dir)
+
+
+def render_system_with_graphviz(system, output_file="system_view.gz"):
+    """ Renders the Drake system (presumably a diagram,
+    otherwise this graph will be fairly trivial) using
+    graphviz to a specified file. """
+    from graphviz import Source
+    string = system.GetGraphvizString()
+    src = Source(string)
+    src.render(output_file, view=False)
+
+
+class BubbleGripperManipulator:
 
     def __init__(self):
         builder = DiagramBuilder()
 
-        self.station = ManipulationStation()
-        self.station.SetupClutterClearingStation()
-        self.station.Finalize()
+        # self.station = ManipulationStation()
+        # self.station.SetupClutterClearingStation()
+        # self.station.Finalize()
 
-        builder.AddSystem(self.station)
-        self.plant = self.station.get_mutable_multibody_plant()
-        # builder.AddSystem(self.station.get_scene_graph())
-        # not sure how to get scene graph
+        self.plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=2e-4)
+        parser = Parser(self.plant)
+        AddPackagePaths(parser)  # Russ's manipulation repo.
+        add_package_paths_local(parser)  # local.
+        directive_file = os.path.join(
+            os.curdir, 'models', 'iiwa.yml')
+        ProcessModelDirectives(LoadModelDirectives(directive_file), self.plant, parser)
+        self.plant.Finalize()
+
+        plant_iiwa_controller, _ = create_iiwa_soft_bubble_plant(
+            self.plant.gravity_field().gravity_vector()
+        )
+        Kp_iiwa = np.array([800, 600, 600, 400, 400, 400, 200])
+        Kd_iiwa = 2 * np.sqrt(Kp_iiwa)
+        Ki_iiwa = np.zeros(7)
+        idc = InverseDynamicsController(plant_iiwa_controller, Kp_iiwa,
+                                        Ki_iiwa, Kd_iiwa, False)
+        builder.AddSystem(idc)
+        model_iiwa = self.plant.GetModelInstanceByName('iiwa')
+        # Connect the inputs and outputs of the controller after adding the controller
+        # to the system
+        builder.Connect(self.plant.get_state_output_port(model_iiwa),
+                        idc.get_input_port_estimated_state())
+        builder.Connect(idc.get_output_port_control(),
+                        self.plant.get_actuation_input_port(model_iiwa))
+
+        q0 = ConstantVectorSource([0, 0, 0, 0, 0, 0, 0, 0., 0., 0., -1.57, 0., 1.57, 0.])
+        builder.AddSystem(q0)
         viz = ConnectMeshcatVisualizer(
-            builder, self.station.get_scene_graph(), zmq_url='tcp://127.0.0.1:6000', prefix="environment")
-
-        # params = MeshcatVisualizerParams()
-        # params.delete_on_initialization_event = False
-        # self.visualizer = MeshcatVisualizerCpp.AddToBuilder(
-        #     builder, self.station.GetOutputPort('query_object'), self.viz, params)
-
-
-        gripper_position = builder.AddSystem(ConstantVectorSource([0.1]))
-
-        builder.Connect(gripper_position.get_output_port(), self.station.GetInputPort('wsg_position'))
-
-
+            builder, scene_graph, zmq_url='tcp://127.0.0.1:6000', prefix="environment")
+        builder.Connect(q0.GetOutputPort('y0'), idc.get_input_port_desired_state())
         self.diagram = builder.Build()
-        self.gripper_frame = self.plant.GetFrameByName('body')  # base frame of iiwa?
-        self.world_frame = self.plant.world_frame()
+        render_system_with_graphviz(self.diagram)
+
+        # builder.AddSystem(self.station)
+        # self.plant = self.station.get_mutable_multibody_plant()
+        # print(self.station.GetGraphvizString())
+
+        # gripper_position = builder.AddSystem(ConstantVectorSource([0.1]))
+
+        # builder.Connect(gripper_position.get_output_port(), self.station.GetInputPort('wsg_position'))
+
+
+        # self.gripper_frame = self.plant.GetFrameByName('body')  # base frame of iiwa?
+        # self.world_frame = self.plant.world_frame()
 
         context = self.diagram.CreateDefaultContext()
         # passing in the object here?
         plant_context = self.diagram.GetMutableSubsystemContext(self.plant, context)
-        station_context = self.diagram.GetMutableSubsystemContext(self.station, context)
+        # station_context = self.diagram.GetMutableSubsystemContext(self.station, context)
 
         q_0 = np.array([0., 0., 0, -1.57, 0, 1.57, 0])
 
-        self.station.SetIiwaPosition(station_context, q_0)
-        self.station.SetIiwaVelocity(station_context, np.zeros(7))
-        self.station.SetWsgPosition(station_context, 0.1)
-        self.station.SetWsgVelocity(station_context, 0)
+        # self.station.SetIiwaPosition(station_context, q_0)
+        # self.station.SetIiwaVelocity(station_context, np.zeros(7))
+        # self.station.SetWsgPosition(station_context, 0.1)
+        # self.station.SetWsgVelocity(station_context, 0)
 
         self.diagram.Publish(context)
-
 
         self.viz = self.diagram.GetSubsystemByName('meshcat_visualizer')
 
@@ -62,18 +119,18 @@ class BubbleGripperManipulator():
         self.viz.reset_recording()
         self.viz.start_recording()
         context = self.diagram.CreateDefaultContext()
-        station_context = self.diagram.GetMutableSubsystemContext(self.station, context)
+        # station_context = self.diagram.GetMutableSubsystemContext(self.station, context)
         q_0 = np.array([0., 0., 0, -1.57, 0, 1.57, 0])
 
-        self.station.SetIiwaPosition(station_context, q_0)
-        self.station.SetIiwaVelocity(station_context, np.zeros(7))
-        self.station.SetWsgPosition(station_context, 0.1)
-        self.station.SetWsgVelocity(station_context, 0)
+        # self.station.SetIiwaPosition(station_context, q_0)
+        # self.station.SetIiwaVelocity(station_context, np.zeros(7))
+        # self.station.SetWsgPosition(station_context, 0.1)
+        # self.station.SetWsgVelocity(station_context, 0)
 
         simulator = Simulator(self.diagram, context)
         simulator.set_target_realtime_rate(1.0)
 
-        duration = 10
+        duration = 0
         simulator.AdvanceTo(duration)
         self.viz.stop_recording()
         self.viz.publish_recording()
@@ -87,4 +144,3 @@ if __name__ == '__main__':
     robot = BubbleGripperManipulator()
 
     robot.start()
-
